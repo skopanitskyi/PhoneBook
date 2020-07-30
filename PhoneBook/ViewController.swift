8305ac6bcf79815828c611a5d754905c4d961663
Serhii <skopanitskyi@gmail.com>
2020-07-30 17:00:30 +0300

    create PhoneBook project

---
+++ PhoneBook/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  PhoneBook
+//
+//  Created by Копаницкий Сергей on 30.07.2020.
+//  Copyright © 2020 Копаницкий Сергей. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
